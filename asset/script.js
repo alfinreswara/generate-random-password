f70520aef77e9302704414c5a9917be3bb901d75
unknown <alfinreswara>
2023-01-01 03:56:58 +0700

    add animation

--- asset/script.js
+++ asset/script.js
@@ -1,51 +1,19 @@
 // Animation
-var htmlView = ` <div class="container">
-      <h1>Generate Random Password</h1>
-      <div class="content">
-        <textarea name="" id="result" cols="25" rows="1"></textarea>
-        <div class="bottom-copy" onclick="copyField()">
-          <svg width="32" height="32" viewBox="0 0 24 24">
-            <path
-              fill="#000000"
-              d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
-          </svg>
-        </div>
-        <div class="bottom-generate">
-          <svg
-            width="32"
-            height="32"
-            viewBox="0 0 16 16"
-            class="generate-animation">
-            <path
-              fill="none"
-              stroke="red"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.5"
-              d="M4.75 10.75h-3m12.5-2c0 3-2.798 5.5-6.25 5.5c-3.75 0-6.25-3.5-6.25-3.5v3.5m9.5-9h3m-12.5 2c0-3 2.798-5.5 6.25-5.5c3.75 0 6.25 3.5 6.25 3.5v-3.5" />
-          </svg>
-        </div>
-      </div>
-      <div class="setting">
-        <input
-          class="inp-number"
-          type="number"
-          id="generate-length"
-          placeholder="8"
-          value="8"
-           />
-        <label class="label ml" for="generate-length"> Length </label>
-        <input class="inp-character" type="checkbox" id="generate-character" />
-        <label class="label ml" for="generate-character">Character</label>
-        <input
-          class="inp-generate-number"
-          type="checkbox"
-          id="generate-number" />
-        <label class="label ml" for="generate-number">Number</label>
-      </div>
-    </div>`;
 
-document.getElementById("root").innerHTML = htmlView;
+var copiedfi = `<div class="text-copied">
+      <span>Text Copied
+      <svg width="32" height="32" viewBox="0 0 24 24">
+        <g fill="none">
+          <path
+            d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z" />
+          <path
+            fill="#fff"
+            d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2Zm0 2a8 8 0 1 0 0 16a8 8 0 0 0 0-16Zm0 11a1 1 0 1 1 0 2a1 1 0 0 1 0-2Zm0-9a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0V7a1 1 0 0 1 1-1Z" />
+        </g>
+      </svg>
+      </span>
+    </div>
+ `;
 
 const bottomGenerate = document.querySelector(".bottom-generate");
 const svgGenerate = document.querySelector(".generate-animation");
@@ -88,7 +56,11 @@ function main() {
 }
 
 bottomGenerate.addEventListener("click", function () {
+  svgGenerate.classList.add("generate-addAnimation");
   resu.value = main();
+  setInterval(function () {
+    svgGenerate.classList.remove("generate-addAnimation");
+  }, 800);
 });
 
 function copyField() {
@@ -96,5 +68,10 @@ function copyField() {
   copyText.select();
   copyText.setSelectionRange(0, 99999);
   document.execCommand("copy");
+  document.getElementById("copied").innerHTML = copiedfi;
+
+  setInterval(function () {
+    document.getElementById("copied").innerHTML = "";
+  }, 2500);
 }
 resu.value = main();
